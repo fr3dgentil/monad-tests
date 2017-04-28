3c8b8cc4d2d938186ab680c0bc659233a51c4fae
Frédéric Gentil <fr3dgentil@gmail.com>
2017-04-28 13:58:42 -0400

    Exemple added

--- IO.js
+++ IO.js
@@ -1,3 +1,5 @@
+// IO operations in a referentially transparent-ish way
+
 class IO {
 	constructor(effect){
 		this.effect = effect;
@@ -13,3 +15,13 @@ class IO {
 	}
 	run(){return this.effect();}
 }
+
+// Usage exemple:
+
+var read = qS => () => document.querySelector(qS).innerHTML;
+var write = qS => val => document.querySelector(qS).innerHTML = val;
+
+var readTitle = read('#title');
+var writeTitle = write('#title');
+
+IO.from(readTitle).map(_.startCase).map(writeTitle).run();
